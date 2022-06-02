574ff5c35df479e54601e623cd4bf52ff49b3e49
Thuli <thulithulzm@gmail.com>
2022-06-02 16:37:49 -0700

    Printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: prints 'Programming is like building a multilingual puzzle'
+ * return 0 - everything went right
+ */
+
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-/** 
- * main = starting point
- * Description: prints 'Programming is like building a multilingual puzzle'
- * return 0 = everything went right
- * /
