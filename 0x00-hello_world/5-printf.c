1a65a9f3078002ce31b4774ef0788e1496c81fbb
Thuli <thulithulzm@gmail.com>
2022-06-02 16:57:56 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * 'main' - interval entry point
+ * main - Entry point
  *
- * printf prints format: with proper grammar, but the outcome is a piece of art
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
+ * Return: Always 0 (Success)
  */
 
 	int main(void)
