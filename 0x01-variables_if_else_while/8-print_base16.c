505328ebb08a106914d20d75f87a584f9d9429ea
Thuli <thulithulzm@gmail.com>
2022-06-05 15:27:40 -0700

    Backward alphabet

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,16 @@
 int main(void)
 {
 	int a;
+	int b;
 
-	for (a = 0; a < 16; ++a)
+	{
+		for (a = 0; a < 10; ++a)
+	putchar(a + '0'); }
 
-	putchar(a + '0');
-
+	{
+	for (b = 'a'; b < 'g'; ++b)
+	putchar(b); }
+	
 	putchar('\n');
 
 		return (0);
