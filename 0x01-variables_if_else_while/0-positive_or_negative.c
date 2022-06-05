cd081d4643829d6f2cb9c9eee0d46233ab0dacfd
Thuli <thulithulzm@gmail.com>
2022-06-05 14:36:13 -0700

    print numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,27 +7,27 @@
  *
 (* Description - assign random number to variable n
  * print the number and if its positive, negative or zero)?
- * 
+ *
  * Return: 0 - Success
  */
 
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
-	
+
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 		printf("%i is positive\n", n);
-	
+
 	else if (n < 0)
 		printf("%i is negative\n", n);
-	
+
 	else
 		printf("%i is zero\n", n);
-	
+
 	return (0);
 }
 
