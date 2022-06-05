fc6440fc7f45d8a5e4d143e031174831c0afdd9f
Thuli <thulithulzm@gmail.com>
2022-06-05 15:13:16 -0700

    Backward alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+(* Description : script that prints A-Z in lowercase
+ * using the putchar function)?
+ *
+ * Return: 0 - Success
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >+ 'a'; --a)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
