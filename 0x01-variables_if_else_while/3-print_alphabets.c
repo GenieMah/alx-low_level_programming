57cdc9330c2d1a8ca142ca1f27707643b215103c
Thuli <thulithulzm@gmail.com>
2022-06-05 12:52:57 -0700

    plint alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+(* Description - script that prints a-z in lowercase
+ * then uppercase)?
+ *
+ * Return: 0 - Success
+ */
+
+int main(void)
+{
+	char c;
+	char a;
+	for (c = 'a'; c <= 'z'; ++c)
+	for (a = 'A'; a <= 'Z'; ++a)
+
+	putchar(c);
+	putchar(a);
+	putchar('\n');
+
+		return (0);
+}
