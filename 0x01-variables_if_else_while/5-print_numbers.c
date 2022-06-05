5791c7e1550e1e24372e9093826599a4b7ca5c84
Thuli <thulithulzm@gmail.com>
2022-06-05 14:25:56 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+(* Description - script that prints a-z in lowercase
+ * then uppercase)?
+ *
+ * Return: 0 - Success
+ */
+
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 10; ++a)
+	printf("%d", i);
+
+	printf("\n");
+
+		return (0);
+}
