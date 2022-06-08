b1c1fe10550538bd3552b0326a3ca055678b9b39
Thuli <thulithulzm@gmail.com>
2022-06-08 11:49:45 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,18 @@
 /**
  * main - entry point
  *
+ * print_alphabet function prints the alphabet
+ *
  * Return: 0 - success
  */
 
 void print_alphabet(void)
 {
 	char a = 'a';
-	
+
 	while (a <= 'z')
 	{
 	_putchar(a);
 	a++; }
 	_putchar('\n');
 }
-
