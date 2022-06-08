5e4ba22b35528221ce155173944e707431f5e186
Thuli <thulithulzm@gmail.com>
2022-06-08 12:00:18 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * print_alphabet function prints the alphabet
+ *
+ * Return: 0 - success
+ */
+
+void print_alphabet(void)
+{
+	int i = 0
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		while (a <= 'z')
+	{
+		_putchar(a);
+		a++; }
+	i++}
+	_putchar('\n');
+}
