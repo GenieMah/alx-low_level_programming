3d85dcc7461f07a85b12449fcddd3c011055292c
Thuli <thulithulzm@gmail.com>
2022-06-08 14:13:42 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * main - entry point
  *
 (* Description: function prints the sign and return value 
  * if conditions are met)?
- *
- * Return: 0 - success
+ * 
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
-	{return 1;
+	{return (1);
 		_putchar('+');}
 	
 	else if (n < 0)
-	{return -1;
+	{return (-1);
 		_putchar('-');}
 	 if (n = 0)
-        {return 0;
+        {return (0);
                 _putchar('0');}
 }
