ca5b78d9c7acdc8109efc2bb1ca9c25f3b8ad379
Thuli <thulithulzm@gmail.com>
2022-06-08 04:58:44 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - entry point
  *
  * Return: 0 - success
- *
  */
 
 int main()
 {
-
+	char a = "_putchar"'
+		_putchar(c):
+		_putchar('\n');
+	return (0);
 }
