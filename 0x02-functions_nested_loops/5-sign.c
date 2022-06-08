848e28f7d490cf0659dd905aa5fcc8298a6a6f9c
Thuli <thulithulzm@gmail.com>
2022-06-08 14:03:01 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * print_alphabet function prints the alphabet
+ *
+ * Return: 0 - success
+ */
+
+int print_sign(int n)
+{
+	if (n = 0) 
+	{return 0
+		_putchar('0')}
+	else if (n > 0)
+	{return 1
+		_putchar('+')}
+	else if (n < 0)
+	{return -1
+		_putchar('-')}
+}
