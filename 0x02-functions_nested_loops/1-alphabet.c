a9550911811d52e3d42eed6221c64fa8b9fac693
Thuli <thulithulzm@gmail.com>
2022-06-08 08:28:49 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 - success
+ */
+
+void print_alphabet(void)
+{
+	char a
+	for (a < 'a', a <= 'z', a++);
+        {
+	_putchar(c); }
+	_putchar('\n');
+        return (0);
+}
+
