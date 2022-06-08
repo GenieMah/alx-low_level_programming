8e50722db31731ff70621e639917f1f029265ed5
Thuli <thulithulzm@gmail.com>
2022-06-08 14:31:48 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * print_alphabet function prints the alphabet
+ *
+ */
+
+int print_last_digit(int)
+{
+	int last = n % 10;
+	
+	return (last);
+}
