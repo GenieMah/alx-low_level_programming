7fbfb216862c2ed495477cd82f5da34a749abab5
Thuli <thulithulzm@gmail.com>
2022-06-08 14:15:26 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,14 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n > '0')
 	{return (1);
 		_putchar('+');}
 	
-	else if (n < 0)
+	else if (n < '0')
 	{return (-1);
 		_putchar('-');}
-	 if (n = 0)
+	 if (n = '0')
         {return (0);
                 _putchar('0');}
 }
