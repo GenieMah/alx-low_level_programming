7d5cdf2d35c3468112ae22d55afda097b7f6a2bb
Thuli <thulithulzm@gmail.com>
2022-06-08 14:46:42 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *Description: computates and returns the absolute number of n
+ *
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+	{
+	return (n);}
+	else if (n < 0)
+	{
+	return (-n);}
+}
