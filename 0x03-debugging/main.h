d753817a30defa9617955a7b1c0267da4601e972
Thuli <thulithulzm@gmail.com>
2022-06-09 11:44:16 -0700

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+void positive_or_negative(int c);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int m, int d, int y);
+int covert_day(int m, int d);
+void print_times_table(int c);
