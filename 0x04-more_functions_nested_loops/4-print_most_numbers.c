e98ed76e47ad822064f45035235e25d0baab329c
Thuli <thulithulzm@gmail.com>
2022-06-10 03:25:07 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * Description: function prints out numbers but 2 and 4
+ *
+ */
+
+void print_numbers(void)
+{
+	char a = 0;
+		while (a = '0'; a<= '9')
+	{
+		if (a != 2 && a != 4)
+		_putchar('0' + a);
+			a++;}
+	_putchar('\n');
+}
