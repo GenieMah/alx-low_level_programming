0bdea31c4db23e4dad640afc8b03e7c350ce9765
Thuli <thulithulzm@gmail.com>
2022-06-10 09:25:48 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int = i
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+		print("FizzBuzz"); }
+	{
+	if (i % 3 == 0)
+		printf("Fizz"); }
+	{
+	if (i % 5 == 0)
+		printf("Buzz"); }
+	{
+	else
+		printf(i); }
+}
