2900af6bce3cf5e0937ec55f4919910dbb670df3
Thuli <thulithulzm@gmail.com>
2022-06-10 05:04:11 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,14 @@
 void print_line(int n)
 {
 	int a;
+
 	if (n <= 0)
 	{
 	_putchar('\n'); }
+
 	else
 	{
-		for (a = 1; a <= n; a++)
-		_putchar('_'); }
+	for (a = 1; a <= n; a++)
+	_putchar('_'); }
+	_putchar('\n');
 }
