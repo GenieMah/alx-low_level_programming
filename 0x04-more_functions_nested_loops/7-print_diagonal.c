38e1a19a6614fb26213a1b2a3671b24dc811112a
Thuli <thulithulzm@gmail.com>
2022-06-10 05:30:10 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ * Description: function prints out numbers
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int c = 0; a;
+
+	if (n > 0)
+
+	{{
+	while (c > n)
+
+	{
+	for (a = 0; a < c; a++)
+	_putchar(' '); }
+
+	_putchar('\\'); }
+	_putchar('\n');
+	c++; }
+
+	else
+	_putcharr('\n');
+}
