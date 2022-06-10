2f9606e55aa62523648fa59d29a0a16b3749cc8a
Thuli <thulithulzm@gmail.com>
2022-06-10 04:03:21 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * Description: function prints out numbers 10 times
+ *
+ */
+
+void more_numbers(void)
+{
+	int a;
+	char b;
+	{
+	for (a = 0; a < 10; a++)
+		{
+			for (b = 1; b <= 14; b++)
+			_putchar(b); }
+	_putchar(a); }
+	_putchar('\n');
+}
