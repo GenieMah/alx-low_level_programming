97c749d8963e5986554d9ae6cc048d4206fa6e54
Thuli <thulithulzm@gmail.com>
2022-06-13 10:44:15 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
+ * print_rev - prints string in reverse
  *
- *
+ * @s - pointer to string
  */
 
 void print_rev(char *s)
 {
 	int c = 0;
-	while (c >= 0)
-	{
-	if (s[c] == '\0')
-		break;
-		c++; }
-	for (c--; c >= 0; c--)
-		_putchar(s[c]);
+
+	while (s[c])
+		c++;
+	while (c--) {
+		_putchar(s[c]); }
+
 	_putchar('\n')
 }
