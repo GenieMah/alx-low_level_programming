5f067e6fe8bdb3660dce250c3e280e39612026ac
Thuli <thulithulzm@gmail.com>
2022-06-13 11:14:51 -0700

    Task 6

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,5 +15,5 @@ void print_rev(char *s)
 	while (c--) {
 		_putchar(s[c]); }
 
-	_putchar('\n')
+	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * rev_string - function reverses string
+ *
+ * @string - pointer of the output
+ */
+
+void rev_string(char *s)
+{
+	int
+	_putchar('\n');
+}
