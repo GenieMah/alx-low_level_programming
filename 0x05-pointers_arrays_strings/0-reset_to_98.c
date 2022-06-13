10de7d304377abc5083b2a438be5908ba8322892
Thuli <thulithulzm@gmail.com>
2022-06-13 09:36:39 -0700

    readme

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 /**
  * reset_to_98 - takes pointer to an int parameter
  *
- * *n - integer pointer
+ * @n - integer
  */
 
-void reset_to_98(int *n) 
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,6 +3,8 @@
 /**
  * swap_int - swaps integer values
  *
+ *  @a - first int
+ *  @b - second int
  */
 
 void swap_int(int *a, int *b)
