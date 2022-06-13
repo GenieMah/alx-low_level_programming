e85d8df83af3213ef27367fcf30b757eecb04838
Thuli <thulithulzm@gmail.com>
2022-06-13 11:36:11 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_array - fucntion frints elements of an array
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+	 for (i < n; i++)
+	 {
+	 	printf("%d", *(a + 1));
+	 if (i != (n - 1))
+	 	printf(", "); }
+	 printf("\n")
+}
