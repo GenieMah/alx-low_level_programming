47c0b7db3667cf50613718416e7df2158add0eb7
Thuli <thulithulzm@gmail.com>
2022-06-14 16:09:57 -0700

    Extra

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,16 +7,16 @@
 
 int _atoi(char *s)
 {
-	int s = 1;
+	int str = 1;
 	unsigned int num = 0;
 	do
 	{
 		if (*s == '-')
-		s *= -1;
+		str *= -1;
 		else if (*s >= '0' && *s <= '9')
 		num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break; }
 	while (*s++);
-	return (num * s);
+	return (num * str);
 }
