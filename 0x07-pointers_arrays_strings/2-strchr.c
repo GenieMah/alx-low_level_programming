a9e4f5c1987dfd8f7f622abafda20d1e964dba6d
Thuli <thulithulzm@gmail.com>
2022-06-20 08:07:04 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int n;
+		for (n = 0; s[n] >= '\0'; n++) {
+			if (s[i] == c) {
+	return (s + i); }
+	}
+	return ('\0');
+}
