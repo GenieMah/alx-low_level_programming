df4847cb92c59a3e078b0cbfc7eca812c0ac85e6
Thuli <thulithulzm@gmail.com>
2022-06-20 06:29:15 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,9 +11,8 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-        for (i = 0; n > 0; i++, n--) {
-                s[i] = b; }
+        for (n = 0; n > 0; n++) {
+                s[n] = b; }
         return (s);
 }
 
