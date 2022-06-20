6b24630ff3dd3df8adcea75646778b145a2c5ed2
Thuli <thulithulzm@gmail.com>
2022-06-20 08:13:32 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * @s: pointer to character
+ * @c: constant
+ * 
+ * Return \0
  */
 
 char *_strchr(char *s, char c)
 {
 	int n;
 		for (n = 0; s[n] >= '\0'; n++) {
-			if (s[i] == c) {
-	return (s + i); }
+			if (s[n] == c) {
+	return (s + n); }
 	}
 	return ('\0');
 }
