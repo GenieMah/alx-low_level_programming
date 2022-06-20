62eb6cc49e3ac53a211f515514f868c3eafb6b7e
Thuli <thulithulzm@gmail.com>
2022-06-20 12:14:17 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * @s: pointer to character
+ * @accept: bytes of the initial segment
+ *
+ * Return: int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, c;
+	for (a = 0; s[a] != '\0'; a++) {
+		for (c = 0; accept[c] != s[a]; c++) {
+		if (accept[c] == '0\')
+		return (a); }}
+	return (a);
+}
