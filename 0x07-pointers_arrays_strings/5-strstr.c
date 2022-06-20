d1681498903cd4f52b1627275b34328be3fc8485
Thuli <thulithulzm@gmail.com>
2022-06-20 12:56:21 -0700

    Task 4

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,9 +12,10 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 	for (i = 0; haystack[i] > '\0'; i++) {
-		for (j = 0; haystack[j] > '\0' && needle[j - i] > '\0'; j++) {
+		for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++) {
 		if (haystack[j] != needle[j - i]) {
 			break; }}
 		if (needle[j - i] == '\0') {
 			return (haystack + i); }}
+	return (0);
 }
