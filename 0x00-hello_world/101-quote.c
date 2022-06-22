ad65537e4ee1b32c3a5ffefa2748ebc28a5ea3d8
Thuli <thulithulzm@gmail.com>
2022-06-22 07:10:37 -0700

    Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  * main: prints out quote
@@ -6,8 +6,8 @@
  * Return 1
  */
 
-char main(void)
+int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
