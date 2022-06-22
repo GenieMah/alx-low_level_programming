ad32cb7c92e0cfa1551bde9a92a9bc32825d638e
Thuli <thulithulzm@gmail.com>
2022-06-22 06:34:38 -0700

    Task 101

---
+++ 101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main: prints out quote
+ *
+ * Return 1
+ */
+
+int main()
+{
+	char i = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	putchar(i);
+		return (1);
+}
