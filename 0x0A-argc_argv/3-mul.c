8bdf96a4a58b1d5e2b882b5d59f676beb66b6828
Thuli <thulithulzm@gmail.com>
2022-06-28 08:21:34 -0700

    Task 0

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+	int i, j, mul;
+	if(argc <=2)
+	{
+		printf("Error\n");
+	return (1); }
+	
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	mul = i * j;
+		printf("%d\n", mul);
+		return (0);
+
+}
