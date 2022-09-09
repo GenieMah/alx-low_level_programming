d8e720cd9e1b63433b876eddba2006dd429bf575
Thuli <thulithulzm@gmail.com>
2022-09-09 12:55:24 -0700

    Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,12 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 9; ++a)
-
-	putchar(a + '0');
-
-	putchar('\n');
+	{
+		putchar(a + '0');
+		putchar(',');
+		putchar(' ');
+	}
+		putchar('\n');
 
 		return (0);
 }
