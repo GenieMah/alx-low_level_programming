b9ec0a486ec2c7ee80141824de3ae0c0397d9eb8
Thuli <thulithulzm@gmail.com>
2022-09-13 05:13:59 -0700

    Putchar

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,15 @@
 /**
  * add - function prints the sum
  *
+ * @a: first parameter
+ * @b: second parameter
+ *
  * Returns the sum of the two inputted values
  */
 
 int add(int a, int b)
 {
 	int sum = a + b;
+
 	return (sum);
 }
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main: starting pouint
+ *
+ * Return: 0 Success
+ */
+
+int main()
+{
+	int n;
+
+	if (n <= 98)
+	{
+		printf("%i", n);
+		n++;
+	}
+	else if (n >= 98)
+	{
+		printf("%i", n);
+		n--;
+	}
+}
