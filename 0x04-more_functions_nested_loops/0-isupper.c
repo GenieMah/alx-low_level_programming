c977b430682aed86d701ba823d8503229cab6fa9
Thuli <thulithulzm@gmail.com>
2022-09-15 12:11:34 -0700

    Is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
+ * _isupper - function prints out whether the inputted value is upper case
  *
+ * @c: initial parameter
  *
+ * Return 1: Success
  */
 
 int _isupper(int c)
