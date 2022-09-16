539cb4561c306a2a913de191fac7727b0e3da925
Thuli <thulithulzm@gmail.com>
2022-09-16 03:52:05 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - function outputs an int onto the console
+ *
+ * @n: initial parameter
+ *
+ *No Return
+ */
+
+void print_number(int n)
+{
+	_putchar(n);
+}
