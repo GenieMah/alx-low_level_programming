25aa727fc755e6615e576ad4dda54e6137ad7e73
Thuli <thulithulzm@gmail.com>
2022-10-04 07:53:00 -0700

    Args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,8 +17,9 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 1; argv[i][j] != '\0'; j++)
-		{	if (argv[i][j] < 0 || argv[i][j] > 9)
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
