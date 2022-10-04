f42af3e247f74892aa72fe9db2b1bfd0c6e47cb0
Thuli <thulithulzm@gmail.com>
2022-10-04 06:23:32 -0700

    Args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-/*
+/**
+ *main - the program's starting point
  *
+ * @argc: argument count
+ * @argv: argument vector
  *
+ * Return: 0 - Success
  */
 
-int main(int __attribute__((unused)) argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
+ * main - program's starting point
  *
+ * @argc: argument count
+ * @argv: arguement vector
  *
- *
+ * Return: 0 - Success
  */
 
-int main(int argc, char* __attribute__((unused)) argv[])
+int main(int argc, char *__attribute__((unused)) argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
