fc5b86bdf157be01c40c5cb52ce92dc00f0bbd70
Thuli <thulithulzm@gmail.com>
2022-10-04 06:47:21 -0700

    Args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,13 +4,13 @@
  * main - program's starting point
  *
  * @argc: argument count
- * @__attribute__((unused)) argv: arguement vector
+ * @argv[]: arguement vector
  *
  * Return: 0 - Success
  */
 
 int main(int argc, char *__attribute__((unused)) argv[])
 {
-	printf("%d\n", argc - 1);
+	printf("%d\n", argc);
 	return (0);
 }
