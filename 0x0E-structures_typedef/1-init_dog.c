16ed9e4ff8e8cc1e0236804c4b8eaa23dc67ee44
Thuli <thulithulzm@gmail.com>
2022-10-15 00:20:48 -0700

    Structs

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include "main.h"
+
+/**
+ *
+ * init_dog - function initialises a var of type struct dog
+ *
+ * @d: name of type struct dog
+ * @name: defined previously
+ * @age: defined previously
+ * @owner: defined previously
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	while (d > '\0')
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include
+/**
+ * struct dog - new structure to define dog
+ *
+ * @name: identifier of dog
+ * @age: age of dog
+ * @ownder: owner of dog
+ *
+ * Description: the struct defines the name age and owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
